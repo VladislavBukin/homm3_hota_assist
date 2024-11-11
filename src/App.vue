5139009d35a2dae06425ede54a026090408316b5
Dist <vlad244244@gmail.com>
2024-11-11 13:10:29 +0300

    _

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <TopBar />
+    <div style="display: flex;">
+      <CastleBar />
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TopBar from './components/TopBar.vue';
+import CastleBar from './components/CastleBar.vue';
+
+export default {
+  components: {
+    TopBar,
+    CastleBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="top-bar">
+		<button @click="goTo('magic')">Магия</button>
+		<button @click="goTo('heros')">Герои</button>
+		<button @click="goTo('artifacts')">Артефакты</button>
+	</div>
+</template>
+
+<script>
+export default {
+	methods: {
+		goTo(route) {
+			this.$router.push(`/${route}`)
+		},
+	},
+};
+</script>
+
+<style>
+.top-bar{
+	display: flex;
+	justify-content: space-around;
+	background-color: #333;
+	padding:10px;
+	color: white;
+}
+button {
+	background: none;
+	color: white;
+	border: none;
+	cursor: pointer;
+}
+button:hover {
+	text-decoration: underline;
+}
+</style>
+
